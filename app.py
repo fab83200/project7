b65f05e38ea70e74fbaf7a49b5b29233c39df77e
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 18:12:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ import random
 #from lime import lime_tabular
 #import streamlit.components.v1 as components
 
-title_text = 'LIME Explainer Dashboard for credit score'
+title_text = 'Credit Score API'
 
 st.markdown(f"<h2 style='text-align: center;'><b>{title_text}</b></h2>", unsafe_allow_html=True)
 st.text("")
 
 # This is the main train table, with TARGET
-app_train = pd.read_csv("../application_train_lite.csv", sep=",")
-for tr in app_train.describe(include='object').columns:
-    app_train[tr]=app_train[tr].fillna((app_train[tr].mode()))
-for ci in app_train.describe().columns:
-    app_train[ci]=app_train[ci].fillna((app_train[ci].median()))
+#app_train = pd.read_csv("../application_train_lite.csv", sep=",")
+#for tr in app_train.describe(include='object').columns:
+#    app_train[tr]=app_train[tr].fillna((app_train[tr].mode()))
+#for ci in app_train.describe().columns:
+#    app_train[ci]=app_train[ci].fillna((app_train[ci].median()))
