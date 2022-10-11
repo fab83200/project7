f22ec65f29ff34047e048df7242fb0367f7bf314
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 18:09:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ title_text = 'LIME Explainer Dashboard for credit score'
 st.markdown(f"<h2 style='text-align: center;'><b>{title_text}</b></h2>", unsafe_allow_html=True)
 st.text("")
 
+../dataset/estadistical.csv
+pd.read_csv("../dataset/estadistical.csv")
+
 # This is the main train table, with TARGET
-application_train_url = "/application_train_lite.csv"
-app_train = pd.read_csv(application_train_url, sep=",")
+app_train = pd.read_csv("../application_train_lite.csv", sep=",")
 for tr in app_train.describe(include='object').columns:
     app_train[tr]=app_train[tr].fillna((app_train[tr].mode()))
 for ci in app_train.describe().columns:
