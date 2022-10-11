060f315debf608237392877186e572143ebf9388
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 18:01:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,14 @@ import pandas as pd
 import numpy as np
 import random
 
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt 
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_repor
-
 #from sklearn.preprocessing import LabelEncoder
-
 #from sklearn.preprocessing import StandardScaler
 #from sklearn.model_selection import train_test_split
-
 #from sklearn.linear_model import LogisticRegression
 #from sklearn.linear_model import LinearRegression
-
 #from sklearn.metrics import accuracy_score
 #from lime import lime_tabular
 #import streamlit.components.v1 as components
-
 
 title_text = 'LIME Explainer Dashboard for credit score'
 
