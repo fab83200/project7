35d474a792a0e483ccb4f73cc811c43f93eed8b1
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-12 13:23:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,11 +47,11 @@ def upload_file(file, variable):
     return variable
     
 # This is the main train table, with TARGET
-#upload_file("application_train_lite.csv", application_train)
-variable = pd.read_csv("application_train_lite.csv", sep=",")
+upload_file(file="application_train_lite.csv", variable=application_train)
+#variable = pd.read_csv("application_train_lite.csv", sep=",")
 
 # This is the main test table, without TARGET
-upload_file(application_test_lite.csv, application_test)
+#upload_file(application_test_lite.csv, application_test)
 
 application_train
 
