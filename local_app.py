6763c78d9593ce9140233e5d39d44a5ba7811498
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-13 15:42:19 +0200

    Update local_app.py

--- local_app.py
+++ local_app.py
@@ -145,7 +145,7 @@ with form:
 						   on_change=None)                         						# Name of the function to use `on_change` repayment_rate_to_annuity
 	amt_annuity = cols[1].number_input("What is the desired credit length?", 	    	# Name of the number_input
 						   key='amt_annuity_widget',                            		# Name of the variable for the data
-						   value=20.0,        											# Sets the default value
+						   value=float(x_valid.loc[idx]['AMT_ANNUITY']),        		# Sets the default value
 						   help=f"Choose a number between {x['AMT_ANNUITY'].min():,} and {x['AMT_ANNUITY'].max():,}", 
 						   on_change=None)                     							# Name of the function to use `on_change`	annuity_to_repayment_rate
 	ext_source_1 = cols[0].number_input("What is the ext_source_1",              		# Name of the number_input
