ee9c70cf191b551eae881ecedb3b4aec97d6bbde
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-13 15:24:16 +0200

    Update local_app.py

--- local_app.py
+++ local_app.py
@@ -138,7 +138,7 @@ with form:
 						   value=float(x_valid.loc[idx]['AMT_CREDIT']),					# Sets the default value
 						   help=f"Choose a number between {x['AMT_CREDIT'].min():,} and {x['AMT_CREDIT'].max():,}", 
 						   on_change=None)                                      		# Name of the function to use `on_change`
-	birthday = st.date_input("What is your birthday?")
+	#birthday = st.date_input("What is your birthday?")
 	payment_rate = st.number_input("What is the desired yearly repayment?", 	    # Name of the number_input
 						   key='payment_rate_widget',                            		# Name of the variable for the data
 						   value=float(x_valid.loc[idx]['AMT_CREDIT'] / x_valid.loc[idx]['AMT_ANNUITY']),        		# Sets the default value
