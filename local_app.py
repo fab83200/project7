1d9613ec9888a493048f8231c33b3cb47be69483
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-13 15:50:01 +0200

    Update local_app.py

--- local_app.py
+++ local_app.py
@@ -136,28 +136,35 @@ with form:
 						   value=float(x_valid.loc[idx]['AMT_CREDIT']),					# Sets the default value
 						   help=f"Choose a number between {x['AMT_CREDIT'].min():,} and {x['AMT_CREDIT'].max():,}", 
 						   on_change=None)                                      		# Name of the function to use `on_change`
-	birthday = cols[1].date_input("What is your birthday?")
-	st.markdown("Fill up either yearly repayment or credit length")
+	
+	birthday = cols[1].date_input("What is your birthday?",								# Name of the number_input
+							value=datetime.date(1984, 6, 3))							# Sets the default value
+	
+	cols[0].markdown("Fill up either yearly repayment or credit length:")
 	payment_rate = cols[0].number_input("What is the desired yearly repayment?", 	    # Name of the number_input
 						   key='payment_rate_widget',                            		# Name of the variable for the data
 						   value=float(x_valid.loc[idx]['AMT_CREDIT'] / x_valid.loc[idx]['AMT_ANNUITY']),        		# Sets the default value
 						   help=f"Choose a number between {x['AMT_ANNUITY'].min():,} and {x['AMT_ANNUITY'].max():,}", 
 						   on_change=None)                         						# Name of the function to use `on_change` repayment_rate_to_annuity
+	
 	amt_annuity = cols[1].number_input("What is the desired credit length?", 	    	# Name of the number_input
 						   key='amt_annuity_widget',                            		# Name of the variable for the data
 						   value=float(x_valid.loc[idx]['AMT_ANNUITY']),        		# Sets the default value
 						   help=f"Choose a number between {x['AMT_ANNUITY'].min():,} and {x['AMT_ANNUITY'].max():,}", 
 						   on_change=None)                     							# Name of the function to use `on_change`	annuity_to_repayment_rate
+	
 	ext_source_1 = cols[0].number_input("What is the ext_source_1",              		# Name of the number_input
 							key='ext_source_1_widget',                                  # Name of the variable for the data
 							value=float(x_valid.loc[idx]['EXT_SOURCE_1']),       		# Sets the default value
 							help=f"Choose a number between {x['EXT_SOURCE_1'].min():,} and {x['EXT_SOURCE_1'].max():,}", 
 							on_change=None)                                      		# Name of the function to use `on_change`
+	
 	ext_source_3 = cols[1].number_input("What is the ext_source_3",             		# Name of the number_input
 							key='ext_source_3_widget',                                  # Name of the variable for the data
 							value=float(x_valid.loc[idx]['EXT_SOURCE_3']),       		# Sets the default value
 							help=f"Choose a number between {x['EXT_SOURCE_3'].min():,} and {x['EXT_SOURCE_3'].max():,}", 
 							on_change=None)                                      		# Name of the function to use `on_change`
+	
 
 	random_element[6] = amt_credit
 	random_element[7] = amt_annuity
