dcc2990f1fe065ee5847e60eccb11fcc2adf0054
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-13 15:32:03 +0200

    Update local_app.py

--- local_app.py
+++ local_app.py
@@ -130,27 +130,28 @@ with form:
 		"""
 		st.session_state.amt_annuity_widget = st.session_state.amt_credit_widget / st.session_state.payment_rate_widget
 	
-	amt_credit = st.number_input("What is the desired loan amount:",       		# Name of the number_input
+	cols=st.columns(2)
+	amt_credit = cols[0].number_input("What is the desired loan amount:",       		# Name of the number_input
 						   key='amt_credit_widget',                                		# Name of the variable for the data
 						   value=float(x_valid.loc[idx]['AMT_CREDIT']),					# Sets the default value
 						   help=f"Choose a number between {x['AMT_CREDIT'].min():,} and {x['AMT_CREDIT'].max():,}", 
 						   on_change=None)                                      		# Name of the function to use `on_change`
-	birthday = st.date_input("What is your birthday?")
-	payment_rate = st.number_input("What is the desired yearly repayment?", 	    # Name of the number_input
+	birthday = cols[1].date_input("What is your birthday?")
+	payment_rate = cols[0].number_input("What is the desired yearly repayment?", 	    # Name of the number_input
 						   key='payment_rate_widget',                            		# Name of the variable for the data
 						   value=float(x_valid.loc[idx]['AMT_CREDIT'] / x_valid.loc[idx]['AMT_ANNUITY']),        		# Sets the default value
 						   help=f"Choose a number between {x['AMT_ANNUITY'].min():,} and {x['AMT_ANNUITY'].max():,}", 
 						   on_change=None)                         # Name of the function to use `on_change` repayment_rate_to_annuity
-	amt_annuity = st.number_input("What is the desired yearly repayment?", 	    # Name of the number_input
+	amt_annuity = cols[1].number_input("What is the desired yearly repayment?", 	    # Name of the number_input
 						   key='amt_annuity_widget',                            		# Name of the variable for the data
 						   help=f"Choose a number between {x['AMT_ANNUITY'].min():,} and {x['AMT_ANNUITY'].max():,}", 
 						   on_change=None)                     	# Name of the function to use `on_change`	annuity_to_repayment_rate
-	ext_source_1 = st.number_input("What is the ext_source_1",              		# Name of the number_input
+	ext_source_1 = cols[0].number_input("What is the ext_source_1",              		# Name of the number_input
 							key='ext_source_1_widget',                                  # Name of the variable for the data
 							value=float(x_valid.loc[idx]['EXT_SOURCE_1']),       		# Sets the default value
 							help=f"Choose a number between {x['EXT_SOURCE_1'].min():,} and {x['EXT_SOURCE_1'].max():,}", 
 							on_change=None)                                      		# Name of the function to use `on_change`
-	ext_source_3 = st.number_input("What is the ext_source_3",             		# Name of the number_input
+	ext_source_3 = cols[1].number_input("What is the ext_source_3",             		# Name of the number_input
 							key='ext_source_3_widget',                                  # Name of the variable for the data
 							value=float(x_valid.loc[idx]['EXT_SOURCE_3']),       		# Sets the default value
 							help=f"Choose a number between {x['EXT_SOURCE_3'].min():,} and {x['EXT_SOURCE_3'].max():,}", 
